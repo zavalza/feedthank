29dfc99b028333f1aaed84cb7d5c3fc8d5a16698
Paul Zavalza <paulz_91@hotmail.com>
2015-02-19 17:28:14 -0600

    logout implemented

--- meteor/client/navigation.js
+++ meteor/client/navigation.js
@@ -5,5 +5,19 @@ Template.navigation.events ({
 			Router.go('editFeedthank');
 		})
 
-	}
+	},
+
+	'click .tryLogout' : function(evt, tmpl)
+	{
+	Meteor.logout(function(err){
+            if (err)
+            {
+              //To do if logout was not successfull
+            }
+            else{
+                Router.go('loginForm');
+              }
+            });
+        return false
+      },
 })

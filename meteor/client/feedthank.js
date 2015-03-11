e42a992bb008cf2e9476c3574480dbcd7cf0cc6a
Paul Zavalza <paulz_91@hotmail.com>
2015-03-11 16:42:50 -0600

    basic format of feedthank

---
+++ meteor/client/feedthank.js
@@ -0,0 +1,16 @@
+ Template.feedthank.helpers({
+
+    image: function(ids)
+        {
+          if (typeof (ids) == 'object')
+          return Images.find({_id:{$in: ids}});
+          else
+          {
+            //alert(typeof (ids)) string
+            return Images.find({_id:ids})
+          }
+          
+        },
+
+ })
+
